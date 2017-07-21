697fb46cc99d4957902b43b836642c7c07d82b26
jkkorea <jongkun7987@gmail.com>
2017-07-21 23:25:52 +0900

    runner 기법 주석 추가

--- src/codinginterview/MyList.java
+++ src/codinginterview/MyList.java
@@ -4,16 +4,23 @@ import java.util.HashSet;
 
 public class MyList {
     private Node head;
-
+    // k가 0부터 시작인지 1부터 시작한지 check
+    
     public Node kthToLast(int k) {
-        if(k < 0){return null;}
+        // k가 음수인지 check
+    	if(k < 0){return null;}
+    	
+    	//차이를 나타내기 위한(두 친구가 달리기 한명이 먼저 다른사람이 나중. 그 간격이 포인트) 즉, runner 기법
         Node n1 = head;
         Node n2 = head;
-
+        
+        // 시간복잡도 고려, n2가 먼저 달림.
         for(int i=0; i<k; i++){
             if(n2==null){return null;}
             n2 = n2.next;
         }
+        
+        // 둘이 같이 달림.
         while(n2.next != null){
             n1 = n1.next;
             n2 = n2.next;
